b7c29a1dd5f6d49d15c31b5cf3adc41f7b955423
jayhklasiq <joshvolx@gmail.com>
2024-07-07 01:26:27 +0000

    completed the microsoft tutorial on blazor games

---
+++ ConnectFour/Program.cs
@@ -0,0 +1,27 @@
+// Program.cs
+using ConnectFour; // Adjust this if App is in a different namespace
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder.Services.AddRazorComponents()
+    .AddInteractiveServerComponents();
+builder.Services.AddSingleton<GameState>();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Error", createScopeForErrors: true);
+    app.UseHsts();
+}
+
+app.UseHttpsRedirection();
+app.UseStaticFiles();
+app.UseAntiforgery();
+
+app.MapRazorComponents<App>()
+    .AddInteractiveServerRenderMode();
+
+app.Run();
